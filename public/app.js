7312096db65049402ec89d74049920fa2f08fb7f
Vasil Genkov Mac <vgenkov15@outlook.com>
2022-11-27 17:33:54 +0200

    somethin

--- public/app.js
+++ public/app.js
@@ -13,6 +13,6 @@ app.post('/subscribe', (req, res) => {
     console.log('hey!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log('Server started!'));
